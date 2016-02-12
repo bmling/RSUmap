432250dd8fc8c267dad65a934d48724d7ce1b6b2
Ling <bmb.banana@gmail.com>
2016-02-12 13:36:02 +0700

    Add User Finish

--- app/src/main/java/com/example/rsu/myproject/LoginActivity.java
+++ app/src/main/java/com/example/rsu/myproject/LoginActivity.java
@@ -5,9 +5,23 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //explicit
+    private MyManage objMyManage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //connected Database
+        objMyManage = new MyManage(this);
+
+        //Test Add Value
+        testAddValue();
+
+    } //Main Method
+
+    private void testAddValue() {
+        objMyManage.addUser("user", "pass", "name");
     }
-}
+} // Main class
